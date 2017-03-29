f26f7e71fd503656cc1c792af1bbc80a5a670b3f
thrinadh <thrinadhn@gmail.com>
2017-03-29 16:39:55 +0530

    commit

--- GLM_LR_B_F.R
+++ GLM_LR_B_F.R
@@ -1,6 +1,6 @@
 
-getwd()
-list.files()
+#getwd()
+#list.files()
 
 AP=read.csv("https://raw.githubusercontent.com/PacktPublishing/R-Data-Mining-Blueprints/master/Chapter%204/Artpiece.csv",header=TRUE)
 ArtPiece=AP
@@ -29,7 +29,7 @@ train_AP=ArtPiece[trainData_AP,]
 test_AP=ArtPiece[-trainData_AP,]
 
 # logistic model
-Model_ArtPiece1=glm(train_AP$IsGood.Purchase~.,family(logit) = binomial,data = train_AP)
+Model_ArtPiece1=glm(train_AP$IsGood.Purchase~.,family = binomial(logit),data = train_AP)
 print(Model_ArtPiece1)
 #model results componemts
 summary(Model_ArtPiece1)
@@ -64,15 +64,45 @@ test_AP$goodl=predict(Model_ArtPiece1,newdata =test_AP,type ="link")
 #BOTH
 
 #auto detection model
-install.packages(c("stepAIC","MASS"),dependencies = TRUE)
-library("stepwise","StepwiseTest","stepR","MASS")
-install.packages("base")
-library("MASS")
+install.packages(c("MASS","stepwise","StepwiseTest","stepR","base"),dependencies = TRUE)
+install.packages(c("plyr","car"),dependencies = TRUE)
+library(stepwise)
+library(StepwiseTest)
+library(stepR)
+library(MASS)
+library(base64)
 library(base)
-
+library(base64enc)
+library(MASS)
+library(base)
+library(plyr)
+library(car)
 fit_step<-stepAIC(Model_ArtPiece1,method="both")
 summary(fit_step)
+vif(fit_step)
+train_AP$prob=predict(fit_step,type="response")
 
-install.packages(c("MASS","plyr","car"),dependencies = TRUE)
-library("MASS","plyr","car")
-browse()
+install.packages("pROC",dependencies = TRUE)        
+library(pROC)
+library(logcondens)
+
+roc_curve=roc(IsGood.Purchase~prob,data=train_AP)
+print(roc_curve)
+plot(roc_curve)
+
+#labels the  prediction result above the the certain threshold and  yes and no 
+# change the threashhold value and check which give the better result.
+train_AP$prob
+train_AP$prob=ifelse(train_AP$prob>0.5,"yes","NO")
+
+# Accuracy
+drop.table(t)
+
+#printing the confusing matrix predicted the actual response on testdata
+tab=table(train_AP$prob,train_AP$IsGood.Purchase)
+print(tab)
+#accuracy
+45453/50867
+
+
+prop.table(tab)
